1d0de181c3bc91d35e01adc477ed32e2d22bf43f
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-17 09:02:39 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import HogBoss from "./HogBoss";
 
 function App() {
-  return (
-    <div className="App">
-      <HogBoss />
-    </div>
-  );
+    return (
+        <div className="App">
+            <HogBoss/>
+        </div>
+    );
 }
 
 export default App;
